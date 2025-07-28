e534012bc187406fcf75b3d69cbc0450cf22659e
Sumit Rajput <123628942+sumitrajput9@users.noreply.github.com>
2025-07-28 13:12:40 +0530

    Update typeorm.config.ts

--- customer-service-backend/src/config/typeorm.config.ts
+++ customer-service-backend/src/config/typeorm.config.ts
@@ -1,12 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); // Load .env file
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'newpassword',
-  database: 'customer_db',
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'newpassword',
+  database: process.env.DB_NAME || 'customer_db',
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
 };
